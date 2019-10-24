f7350bb3089b00672556d76ebf337c0c50c6c777
loisgenesis <loisgenesis18@gmail.com>
2019-10-25 00:03:36 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,169 @@
+import 'package:flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Loan Calculator',
+    home: LoanApp(),
+  ));
+}
+
+class LoanApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoanAppState();
+  }
+}
+
+class _LoanAppState extends State<LoanApp> {
+  final double padding = 10.0;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  bool _autoValidate = false;
+  double principal;
+  double interest;
+  double months;
+  TextEditingController principalEditingController =
+      new TextEditingController();
+  TextEditingController interestEditingController = new TextEditingController();
+  TextEditingController monthsEditingController = new TextEditingController();
+  double monthlyResult = 0;
+  double totalResult = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text('Loan Calculator'),
+          backgroundColor: Colors.green,
+        ),
+        body: Form(
+          key: _formKey,
+          autovalidate: _autoValidate,
+          child: SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.all(padding * 2),
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: principalEditingController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Principal amount',
+                      hintText: 'Enter Principal amount e.g. 500000',
+                    ),
+                    validator: _validateValue,
+                    onSaved: (value) {
+                      principal = double.parse(value);
+                    },
+                  ),
+                  SizedBox(
+                    height: padding,
+                  ),
+                  TextFormField(
+                    controller: interestEditingController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Rate of Interest',
+                      hintText: 'In percentage e.g 10',
+                    ),
+                    validator: _validateValue,
+                    onSaved: (value) {
+                      interest = double.parse(value);
+                    },
+                  ),
+                  SizedBox(
+                    height: padding,
+                  ),
+                  TextFormField(
+                    controller: monthsEditingController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Term in months',
+                      hintText: 'e.g 2',
+                    ),
+                    validator: _validateValue,
+                    onSaved: (value) {
+                      months = double.parse(value);
+                    },
+                  ),
+                  SizedBox(height: 40),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: RaisedButton(
+                          child: Text('Calculate'),
+                          onPressed: _handleSubmitted,
+                        ),
+                      ),
+                      Expanded(
+                        child: RaisedButton(
+                          child: Text('Reset'),
+                          onPressed: resetValues,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(padding * 2),
+                    child: Text(
+                        'Monthly payment of : ${monthlyResult.floorToDouble()}'),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(padding * 2),
+                    child:
+                        Text('Total payment : ${totalResult.floorToDouble()}'),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+
+  String _validateValue(String value) {
+    if (value == null || value.isEmpty) return "This field is required";
+    return null;
+  }
+
+  void showInSnackBar(String value) {
+    Flushbar(
+      message: value,
+      backgroundColor: Colors.red,
+      borderRadius: 10,
+      margin: EdgeInsets.all(20),
+      duration: Duration(seconds: 3),
+    )..show(context);
+//    _scaffoldKey.currentState
+//        .showSnackBar(new SnackBar(content: new Text(value),backgroundColor: MyColors.redBg,));
+  }
+
+  resetValues() {
+    principalEditingController.text = "";
+    interestEditingController.text = "";
+    monthsEditingController.text = "";
+  }
+
+  void _handleSubmitted() {
+    final FormState form = _formKey.currentState;
+    if (!form.validate()) {
+      _autoValidate = true; // Start validating on every change.
+      showInSnackBar("This field is required");
+      return;
+    } else
+      form.save();
+    calculateValues();
+  }
+
+  calculateValues() {
+    double oldInterest = principal * interest;
+    double newInterest = oldInterest / 100;
+    double monthlyPayment = newInterest * months;
+    setState(() {
+      totalResult = principal + monthlyPayment;
+      monthlyResult = totalResult / months;
+    });
+  }
+}
